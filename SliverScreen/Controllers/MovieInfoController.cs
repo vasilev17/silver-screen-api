d8b85e1dd631e408e0960c8fcee42e7e6e08eede
Valentin Vasilev <VVVasilev@nemetschek.bg>
2022-01-27 15:46:04 +0200

    Added movie info page controller and service

---
+++ SliverScreen/Controllers/MovieInfoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SliverScreen.Controllers
+{
+    public class MovieInfoController : Controller
+    {
+        
+    }
+}
