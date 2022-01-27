206daf8441ecb8ab1dc82f7fb9875123f4cc0fd5
Boris Stefanov <bstefanov@nemetschek.bg>
2022-01-27 16:59:00 +0200

    Added example with comments

--- SliverScreen/Controllers/AdministrationManagmentController.cs
+++ SliverScreen/Controllers/AdministrationManagmentController.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace SliverScreen.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class AdministrationManagmentController : Controller
     {
+        static List<string> comments = new List<string>();
+
+        [HttpPost]
+        [Route("PreloadComments")]
+        public void PreloadComments()
+        {
+            comments.Add("I liked that film!");
+            comments.Add("Go fuck yourself! I hate it!!!!");//Harrasment comment example
+            comments.Add("Chill dude! This site is litteraly an IMDb ripoff.");
+        }
+
+        [HttpGet]
+        [Route("CommentManagment")]
+        public List<string> GetComments()
+        {            
+            return comments;
+        }
+
+        [HttpPost]
+        [Route("CommentManagment")]
+        public string PostComment(string comment)
+        {
+            comments.Add(comment);
+            return "Comment added successfuly!";
+        }
+
+        [HttpPut]
+        [Route("CommentManagment")]
+        public string EditComment(int id, string comment)
+        {
+            comments[id] = comment;
+            return "Comment edited successfuly!";
+        }
+
+        [HttpDelete]
+        [Route("CommentManagment")]
+        public string DeleteComment(int id)
+        {
+            comments.RemoveAt(id);
+            return "Comment deleted successfuly!";
+        }
+
     }
 }
