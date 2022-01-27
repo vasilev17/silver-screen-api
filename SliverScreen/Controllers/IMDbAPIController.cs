55bce49c317ce91be0948ae4be6bb56a66c69e81
Kristian Valchev <KValchev@nemetschek.bg>
2022-01-27 17:04:00 +0200

    added rest endpoints

--- SliverScreen/Controllers/IMDbAPIController.cs
+++ SliverScreen/Controllers/IMDbAPIController.cs
@@ -8,6 +8,29 @@ namespace SliverScreen.Controllers
 {
     public class IMDbAPIController : Controller
     {
-        
+        [HttpGet]
+        [Route("TestNotification")]
+        public string GetTestNotification()
+        {
+            return "Get method";
+        }
+        [HttpDelete]
+        [Route("TestNotification")]
+        public string DeleteTestNotification()
+        {
+            return "Delete method";
+        }
+        [HttpPut]
+        [Route("TestNotification")]
+        public string PutTestNotification()
+        {
+            return "Put method";
+        }
+        [HttpPost]
+        [Route("TestNotification")]
+        public string PostTestNotification()
+        {
+            return "Post method";
+        }
     }
 }
