01d3b793d2756d986623cd4fa612997b2b1459a7
Valentin Vasilev <VVVasilev@nemetschek.bg>
2022-01-27 17:07:04 +0200

    Added 4 rest endpoints

--- SliverScreen/Controllers/MovieInfoController.cs
+++ SliverScreen/Controllers/MovieInfoController.cs
@@ -8,6 +8,32 @@ namespace SliverScreen.Controllers
 {
     public class MovieInfoController : Controller
     {
-        
+        [HttpGet]
+        [Route("TestNotification")]
+        public string GetTestNotification()
+        {
+            return "MovieInfo Get method";
+        }
+
+        [HttpDelete]
+        [Route("TestNotification")]
+        public string DeleteTestNotification()
+        {
+            return "MovieInfo Delete method";
+        }
+
+        [HttpPut]
+        [Route("TestNotification")]
+        public string PutTestNotification()
+        {
+            return "MovieInfo Put method";
+        }
+
+        [HttpPost]
+        [Route("TestNotification")]
+        public string PostTestNotification()
+        {
+            return "MovieInfo Post method";
+        }
     }
 }
