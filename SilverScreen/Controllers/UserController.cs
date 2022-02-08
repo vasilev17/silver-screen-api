34713e3e4d47ba12f17e2da031ff86dc03c025b1
Bogomil Slavkov <bslavkov@nemetschek.bg>
2022-02-08 16:21:50 +0200

    login + register logic

--- SilverScreen/Controllers/UserController.cs
+++ SilverScreen/Controllers/UserController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using SilverScreen.Models;
 using SilverScreen.Models.Tables;
 using SilverScreen.Services;
 using System;
@@ -21,11 +23,47 @@ namespace SilverScreen.Controllers
 
         [HttpGet]
         [Route("UserGetRequest")]
+        [Authorize]
         public User GetUserDetails(int userID)
         {
             UserService userService = new UserService(Config);
             return userService.GetUserByID(userID);
         }
-      
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult Login([FromBody] Login login)
+        {
+            UserService userService = new UserService(Config);
+            IActionResult response = Unauthorized();
+            var user = userService.AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var tokenString = userService.GenerateJSONWebToken(login);
+                response = Ok(new { token = tokenString });
+            }
+
+            return response;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("Register")]
+        public IActionResult Register([FromBody] Login login)
+        {
+            UserService userService = new UserService(Config);
+            var user = userService.RegisterUser(login);
+
+            if (user != null)
+            {
+                var tokenString = userService.GenerateJSONWebToken(login);
+                return Ok(new { token = tokenString });
+            }
+            return Ok(new { ErrorMessage = "Error" });
+            //return 500
+        }
+
     }
 }
