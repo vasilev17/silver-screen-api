a692462efa4b4c703640d7e86b528b4868f38735
Boris Stefanov <bstefanov@nemetschek.bg>
2022-02-17 01:58:02 +0200

    Some updates made to the controller and added few models, because it will create problems later.

---
+++ SilverScreen/Models/BasicNotificationRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SilverScreen.Models
+{
+    public class BasicNotificationRequest
+    {
+        public int notificationId { get; set; }
+    }
+}
---
+++ SilverScreen/Models/RecommendMovieToUserRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SilverScreen.Models
+{
+    public class RecommendMovieToUserRequest
+    {
+        public int friendId { get; set; }
+        public int movieId { get; set; }
+        public string message { get; set; }
+    }
+}
