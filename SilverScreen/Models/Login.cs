6128c0e5bcf9f808b3275111999f1f25fd36944e
Bogomil Slavkov <bslavkov@nemetschek.bg>
2022-03-01 16:09:58 +0200

    confirm password property

--- SilverScreen/Models/Login.cs
+++ SilverScreen/Models/Login.cs
@@ -10,5 +10,7 @@ namespace SilverScreen.Models
         public string Email { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+
+        public string confirmPassword { get; set; }
     }
 }
