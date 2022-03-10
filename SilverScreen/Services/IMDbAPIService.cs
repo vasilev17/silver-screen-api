d6bc6ddeef7cc48641da1995641beb351fae8974
Danail D .Dimitrov <dddimitrov@nemetschek.bg>
2022-03-10 16:12:50 +0200

    changed the order of the api keys

--- SilverScreen/Services/IMDbAPIService.cs
+++ SilverScreen/Services/IMDbAPIService.cs
@@ -246,7 +246,7 @@ namespace SilverScreen.Services
             }
             int addedMovies = 0; 
             var movieCount = count;
-            string[] apiKeys = { "k_faxyw40f", "k_t9h2vl7k", "k_mfd5skue", "k_xahgruqu", "k_44lmaclu" , "k_k15dcusr" , "k_zrj40392" };
+            string[] apiKeys = { "k_zrj40392", "k_t9h2vl7k", "k_mfd5skue", "k_xahgruqu", "k_44lmaclu" , "k_k15dcusr" , "k_faxyw40f" };
             int keyCount = 0;
             string API_KEY = apiKeys[keyCount];
             string url = "https://imdb-api.com/API/AdvancedSearch/" + API_KEY;
